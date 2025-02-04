60a85d47554da9a6933cda8199def30f4a4770e4
Bryan Choate <bryan.choate@gmail.com>
2025-02-04 17:19:30 -0500

    Remove git subtrees to replace with microservice paths

--- user-service/app/db/repositories/role_repo.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.db.models import Role
-from app.db.repositories.base_repo import BaseRepository
-
-
-class RoleRepository(BaseRepository[Role]):
-    def __init__(self, db: AsyncSession):
-        super().__init__(model=Role, db=db)
